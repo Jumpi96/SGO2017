987b01d95af7b7865551a97be807ebb49d4e7eab
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2017-09-25 10:12:38 -0300

    Proyecto nuevo arq WebAPI - Modelos From DB cargados

---
+++ Producto/SGO/SGO/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SGO.MvcApplication" Language="C#" %>
---
+++ Producto/SGO/SGO/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SGO.Startup))]
+namespace SGO
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
