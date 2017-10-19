6532d46b60daeec1a48c8047cb0976f5314a063c
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2017-10-18 21:15:10 -0300

    Quitando de index proyecto

--- Proyecto/Django/SGOapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SgoappConfig(AppConfig):
-    name = 'SGOapp'
--- Proyecto/Django/SGOapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-	url(r'^(?P<id_obra>[0-9]+)/$', views.ver_obra, name='ver_obra')
-]
